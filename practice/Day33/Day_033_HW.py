276c6cc7dc86e443158cad83d497734acf4aedd3
pony <pony8674312@gmail.com>
2019-10-11 15:00:58 +0800

    upload Day33 homework

---
+++ practice/Day33/Day_033_HW.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## 練習時間
+
+# 請觀看李宏毅教授以神奇寶貝進化 CP 值預測的範例，解說何謂機器學習與過擬合。並回答以下問題
+
+# [youtube](https://www.youtube.com/watch?v=fegAeph9UaA)
+
+# ### 1. 模型的泛化能力 (generalization) 是指什麼？ 
+# ### 2. 分類問題與回歸問題分別可用的目標函數有哪些？
+
+
+#1 : 指模型在測試集上的預測能力
+#2 : 回歸問題 : MSE、RMSE、MAE
+#2 : 分類問題 : coss entropy
